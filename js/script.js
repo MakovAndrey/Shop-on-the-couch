a422c0d28312a3576a1b3759bd4e328a9b92b222
MakovAndrey <mayc52@mail.ru>
2022-01-28 16:46:30 +0300

    catalog.html starts

--- js/script.js
+++ js/script.js
@@ -1,17 +1,20 @@
 const toggle = document.getElementById("menu-btn");
-
 const nav = document.getElementById("navigate");
 
 toggle.addEventListener("click", () => {
-    toggle.classList.add("display-block");
-    nav.classList.toggle("display-block");
+    nav.classList.toggle("nav-on");
 });
 
 const headerSearchBtn = document.getElementById("header__search_btn");
-
 const headerSearchInp = document.getElementById("header__search_inp");
 
 headerSearchBtn.addEventListener("click", () => {
-    // headerSearchBtn.classList.add("header__search_inp-on");
     headerSearchInp.classList.toggle("header__search_inp-on");
+});
+
+const filterBtn = document.getElementById("filter-btn");
+const filter = document.getElementById("filter");
+
+filterBtn.addEventListener("click", () => {
+    filter.classList.toggle("catalog__filter-list-on");
 });
